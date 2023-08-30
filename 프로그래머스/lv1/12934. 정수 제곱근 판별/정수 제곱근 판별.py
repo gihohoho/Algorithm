b9b6364f4aa8bbf51697152352f2a3235c41ed8f
gihohoho <47467182+gihohoho@users.noreply.github.com>
2023-08-30 09:11:30 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,12 +1,12 @@
 def solution(n):
-    if n < 1:
+    if n<1:
         return -1
-
-    x = 1
-    while x * x < n:
+    
+    x=1
+    while x*x < n:
         x += 1
-
-    if x * x == n:
-        return (x + 1) ** 2
+        
+    if x*x == n:
+        return int((x+1)*(x+1))
     else:
         return -1
